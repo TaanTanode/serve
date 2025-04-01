834b70e11ef11f5cc24785b5bf7ae728d1bfb9bc
TaanTanode <taantanode1428@gmail.com>
2025-04-01 14:04:58 +0700

    update prisma

--- config/prisma.js
+++ config/prisma.js
@@ -1,4 +1,13 @@
-const { PrismaClient } =require('@prisma/client')
-const prisma = new PrismaClient()
+const { PrismaClient } = require('@prisma/client')
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+const globalForPrisma = globalThis || {}
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 module.exports = prisma
